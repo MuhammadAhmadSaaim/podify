23c70ea6a5efc9a634665ad6412b4f2cd2b6f628
Ahmad <131032658+MuhammadAhmadSaaim@users.noreply.github.com>
2024-09-15 01:28:04 +0500

    Clerk Auth Done

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -1,9 +1,16 @@
+'use client';
+
 import Link from "next/link";
 import React from 'react'
 import Image from 'next/image'
 import { sidebarLinks } from "@/constants";
+import { usePathname, useRouter } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 const LeftSidebar = () => {
+    const pathname = usePathname();
+    const router = useRouter();
+
     return (
         <section className="left_sidebar">
             <nav className="flex flex-col gap-6">
@@ -13,8 +20,11 @@ const LeftSidebar = () => {
                 </Link>
 
                 {sidebarLinks.map(({ route, label, imgURL }) => {
+                    const isActive = pathname === route || pathname.startsWith(`${route}/`);
                     return (
-                        <Link href={route} key={label} className="flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start">
+                        <Link href={route} key={label} className={cn("flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start", {
+                            'bg-nav-focus border-r-4 border-orange-1': isActive
+                        })}>
                             <Image src={imgURL} alt="label" width={24} height={24} />
                             <p>{label}</p>
                         </Link>
